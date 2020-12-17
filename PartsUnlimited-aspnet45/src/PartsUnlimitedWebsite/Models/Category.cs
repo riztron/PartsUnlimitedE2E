7db50d0d9365e270c23942820d860a18903c1ef6
riztron <riztron@gmail.com>
2020-12-17 08:30:33 +0000

    This is my first commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    //This is a modified commment
     public class Category
     {
         public int CategoryId { get; set; }
